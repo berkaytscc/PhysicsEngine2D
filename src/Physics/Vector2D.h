a40ca98c14b86942c58d12ea701af35321f0564a
classworm <classworm@icloud.com>
2022-12-05 00:05:18 +0300

    console project added
    ToString method added for vector struct
    operator overloading added
    created a console project for testing

--- src/Physics/Vector2D.h
+++ src/Physics/Vector2D.h
@@ -1,6 +1,8 @@
 #ifndef VEC2_H
 #define VEC2_H
 
+#include <string>
+
 /// <summary>
 /// Representation of 2D vectors and points.
 /// This structure is used in some places to represent 2D positions and vectors
@@ -19,15 +21,15 @@ struct Vector2D {
 	//		Search for that can it be done any better. Also the usage of 'const' and 'referance 
 	//		parameters' should be implemented when neccessary.
 	
-	//Vector2D() = default;
+	Vector2D();
 	Vector2D(float x, float y);
 
 	~Vector2D() = default;
-
+																							
 	void Add(Vector2D v);								// v1.add(v2);
 	void Sub(Vector2D v);								// v1.Sub(v2);
 	void Scale(float n);								// v1.Scale(x);
-	void Set(float newX, float newY);					// v1.Set(newX, newY);
+	void Set(float newX, float newY);					// v1.Set(newX, newY);				
 	Vector2D Rotate(float angle);						// v1.Rotate(angle);
 
 	float Magnitude();  								// v1.Magnitude();
@@ -38,8 +40,9 @@ struct Vector2D {
 	/// <param name="v"> --> first vector</param>
 	/// <param name="v2"> --> second vector</param>
 	/// <returns></returns>
-	static float Angle(Vector2D v, Vector2D v2);		// Vector2D.Angle(v,v2);
 	
+	float Angle(Vector2D v, Vector2D v2);				// Vector2D.Angle(v,v2);
+
 	float Dot(Vector2D v);								// v1.Dot(v2);
 	float Cross(Vector2D v);							// v1.Cross(v2);
 	
@@ -55,6 +58,25 @@ struct Vector2D {
 	Vector2D UnitVector();								// v1.UnitVector();
 	Vector2D Perpendicular();							// v1.Perpendicular();
 
-	bool isEqual(Vector2D v);							// v1.isEqual(v);							
+	bool isEqual(Vector2D v);							// v1.isEqual(v);		
+	
+#pragma region Operator Overloading
+	//operator = (Vector2D v);							// v1 = v2
+	bool operator == (Vector2D v);
+	bool operator != (Vector2D v);
+
+	Vector2D operator+ (Vector2D v);					// v1 + v2
+	Vector2D operator- (Vector2D v);					// v1 - v2
+	Vector2D operator* (float n);						// v1 * n
+	Vector2D operator/ (float n);						// v1 / n
+	Vector2D operator- ();								// v1 - v2
+	
+	Vector2D operator+= (Vector2D v);					// v1 += v2
+	Vector2D operator-= (Vector2D v);					// v1 -= v2
+	Vector2D operator*= (float n);						// v1 *= n
+	Vector2D operator/= (float n);						// v1 /= n
+#pragma endregion
+
+	std::string ToString();
 };
 #endif
