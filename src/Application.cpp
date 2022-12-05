9d4f2e04df82c3f871a5539a3e50a5d50c3f43f4
tunacan <tunauzunokur@gmail.com>
2022-12-06 02:13:00 +0300

    Graphics, Application headers editted

--- src/Application.cpp
+++ src/Application.cpp
@@ -1,28 +1,55 @@
 #include "Application.h"
+#include "Graphics.h"
+#include "Physics/Constants.h"
 
 bool Application::isRunning() {
 	return running;
 }
 
 void Application::Setup() {
-	//TODO: Open window --> OpenWindow()
-	//		setup objects in the scene
+	running = Graphics::OpenWindow();
 }
 
 void Application::Input() {
-	//TODO: Input processing
+	
+	SDL_Event event;
+
+	while (SDL_PollEvent(&event)) {
+		switch (event.type)
+		{
+		case SDL_QUIT:
+			running = false;
+			break;
+
+		case SDL_KEYDOWN:
+			if (event.key.keysym.sym == SDLK_ESCAPE)
+				running = false;
+			if (event.key.keysym.sym == SDLK_d)
+				debug = !debug;
+			break;
+
+		default:
+			break;
+		}
+	}
 }
 
 void Application::Update() {
-	//TODO: Update all objects in the scene
+	
+	Graphics::ClearScreen();
+	static int timePreviousFrame;
+	int timeToWait = DESIRED_DELTA - (SDL_GetTicks() - timePreviousFrame);
+
+	if (timeToWait > 0)
+		SDL_Delay(timeToWait);
 }
 void Application::Render() {
+	Graphics::RenderFrame();
 	//TODO: Render function is called several times per second to draw objects
 }
 /// <summary>
 /// Call this method to delete objects in the scene
 /// </summary>
 void Application::Destroy() {
-	//TODO: Delete objects and close the window
-	//		Close window --> CloseWindow()
+	Graphics::CloseWindow();
 }
--- src/Graphics.h
+++ src/Graphics.h
@@ -1,7 +1,31 @@
 #ifndef GRAPHICS_H
 #define GRAPHICS_H
 
-// SDL2 libraries and Physics/... libraries are included
+
+#include <SDL.h>
+
+
+
+struct Graphics
+{
+public:
+
+	static int windowWidth;
+	static int windowHeight;
+	//static SDL_Renderer* renderer;
+	//static SDL_Window* window;
+
+	static bool OpenWindow();
+	static void RenderFrame();
+	static void ClearScreen();
+	static void CloseWindow();
+
+	static int Width();
+	static int Height();
+
+};
+
+
 
 #endif
 
